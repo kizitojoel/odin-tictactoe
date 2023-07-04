1fab2b0e29a4295be0acc12f5b524673665b015d
Joel Kizito <kizitojoel16@gmail.com>
2023-07-04 18:02:34 +0300

    Add game logic

--- script.js
+++ script.js
@@ -3,11 +3,90 @@
  */
 const gameboard = (() => {
     var array = [
-        ['X', 'O', 'O'],
-        ['O', 'X', 'X'],
-        ['X', 'O', 'X']
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
     ];
-    return {array};
+
+    const reset = () => {
+        displayController.turn = 'X';
+        console.clear();
+        for (let i = 0; i < 3; i++)
+        {
+            for (let j = 0; j < 3; j++)
+            {
+                array[i][j] = ''; 
+            }
+        }
+        render(array);
+    };
+
+    const render = (inputArray) => {
+        const grid = document.getElementById("grid");
+        for (let i = 0; i < 3; i++)
+        {
+            for (let j = 0; j < 3; j++)
+            {
+                grid.children[i].children[j].textContent = inputArray[i][j];
+            }
+        }
+    }
+
+    function checkWinner(){
+        // Check horizontally
+        for (let i = 0; i < 3; i++)
+        {
+            let row = array[i]
+            if (row[0] === row[1] && row[1] === row[2] && row[0])
+            {
+                return (row[0]);
+            }
+        }
+
+        // Check vertically
+        for (let col = 0; col < 3; col++)
+        {
+            if (array[0][col] && array[0][col] === array[1][col] && array[1][col] === array[2][col])
+            {
+                return (array[0][col]);
+            }
+        }
+        
+        // Check diagonals
+        if (array[0][0] && array[0][0] === array[1][1] && array[1][1] === array[2][2])
+        {
+            return (array[0][0]);
+        }
+        if (array[0][2] && array[0][2] === array[1][1] && array[1][1] === array[2][0])
+        {
+            return (array[0][2]);
+        }
+    }
+
+    function checkDraw(){
+        for (let i = 0; i < 3; i++)
+        {
+            for (let j = 0; j < 3; j++)
+            {
+                if (!array[i][j])
+                {
+                    return (false);
+                }
+            }
+        }
+        if (!checkWinner())
+        {
+            return (true);
+        }
+    }
+
+    return {
+        array, 
+        reset, 
+        render,
+        checkWinner,
+        checkDraw
+    };
 })();
 
 const displayController = (() => {
@@ -17,27 +96,61 @@ const displayController = (() => {
      * @param {string} turn This takes in the character that just played
      * @returns The character that needs to play next
      */
-    const toggleTurn = (turn) => {
-        return (turn === 'X') ? 'O' : 'X';
+    const toggleTurn = (currentTurn) => currentTurn === 'X' ? 'O' : 'X';
+    return {
+        turn, 
+        toggleTurn
     };
-    return {turn, toggleTurn};
 })();
 
-function playerFactory(name, character){
-    return {name, character};
+function playerFactory(character){
+    const _char = character;
+    const getChar = () => _char;
+    return {
+        getChar
+    }
 }
 
-function renderGameboard(array){
-    const grid = document.getElementById("grid");
+monitorGameboard = (() => {
+    const sections = document.getElementsByClassName("grid-row");
+
+    function _printCharacter(e){
+        var target = e.target;
+        var i = Number(target.parentElement.dataset.row);
+        var j = Number(target.dataset.col);
+
+        if (gameboard.checkWinner() || gameboard.checkDraw())
+        {
+            return;
+        }
+
+        if (!gameboard.checkWinner() && !gameboard.checkDraw() && gameboard.array[i][j] !== 'X' && gameboard.array[i][j] !== 'O')
+        {
+            gameboard.array[i][j] = displayController.turn;
+            gameboard.render(gameboard.array);
+            displayController.turn = displayController.toggleTurn(displayController.turn);
+        }
+        let winner = null;
+        if (winner = gameboard.checkWinner())
+        {
+            console.log(`Winner is ${winner}`);
+            const timeout = setTimeout(gameboard.reset, 3000);
+        }
+        if (gameboard.checkDraw())
+        {
+            console.log('This is a draw');
+            const timeout = setTimeout(gameboard.reset, 3000)
+        }
+    }
+
     for (let i = 0; i < 3; i++)
     {
         for (let j = 0; j < 3; j++)
         {
-            grid.children[i].children[j].textContent = array[i][j];
+            sections[i].children[j].addEventListener('click', _printCharacter);
         }
     }
-}
+})();
 
-renderGameboard(gameboard.array);
 
 
